a1db11e8e1f7b1aa1398ee302f4c3215d29d5bba
Richard Liu <richardliux23@gmail.com>
2023-04-16 06:07:55 +0800

    Starting using chi to route our app

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/dawnaliens/silver-telegram.git
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.8 // indirect
+require github.com/go-chi/chi/v5 v5.0.8
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
@@ -33,28 +34,18 @@ func questionHandler(w http.ResponseWriter, r *http.Request) {
 //	}
 //}
 
-type Router struct {
-}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		questionHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
 func main() {
 	//var router http.HandlerFunc
 	//router = pathHandler
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", questionHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	//http.HandleFunc("/", homeHandler)
 	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on: 3000....")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
